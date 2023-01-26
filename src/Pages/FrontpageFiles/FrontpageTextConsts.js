ce4f3145a01a91f776775a9738d1c834aafc34e1
MirkusX <marcusk.kodehode@gmail.com>
2023-01-26 09:38:05 +0100

    first commit

--- src/Pages/FrontpageFiles/FrontpageTextConsts.js
+++ src/Pages/FrontpageFiles/FrontpageTextConsts.js
@@ -1,17 +1,17 @@
-import gustav from "../../Images/gustav.avif";
-import joseph from "../../Images/joseph.avif";
-import vincent from "../../Images/vincent.avif";
-import avatar1 from "../../Images/avatar1.avif";
-import avatar2 from "../../Images/avatar2.avif";
-import avatar3 from "../../Images/avatar3.avif";
+import postimage from "../../Images/post-image.png";
+import postimage1 from "../../Images/post-image1.png";
+import postimage2 from "../../Images/post-image2.png";
+import avatar from "../../Images/avatar.avif";
+import avatar1 from "../../Images/avatar2.avif";
+import avatar2 from "../../Images/avatar3.avif";
 
-const posts = [
+export const posts = [
   {
     name: "Vincent van Gogh",
     username: "vincey1853",
     location: "Zundert, Netherlands",
-    avatar: avatar1,
-    post: vincent,
+    avatar: avatar,
+    post: postimage,
     comment: "just took a few mushrooms lol",
     likes: 21,
   },
@@ -19,8 +19,8 @@ const posts = [
     name: "Gustave Courbet",
     username: "gus1819",
     location: "Ornans, France",
-    avatar: avatar2,
-    post: gustav,
+    avatar: avatar1,
+    post: postimage1,
     comment: "i'm feelin a bit stressed tbh",
     likes: 4,
   },
@@ -28,8 +28,8 @@ const posts = [
     name: "Joseph Ducreux",
     username: "jd1735",
     location: "Paris, France",
-    avatar: avatar3,
-    post: joseph,
+    avatar: avatar2,
+    post: postimage2,
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
     likes: 152,
